930ae7ba7de9c69223bf69ba9589f5e6dac9d563
Moutaz Ali <moutaz.ali.dev@gmail.com>
2024-05-29 10:47:47 +0300

    create signup functionality and add emails to strapi db

---
+++ app/register/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RegisterPage = () => {
+  return <div className="mt-[9rem]">RegisterPage</div>;
+};
+
+export default RegisterPage;
--- components/SubscripeCard.tsx
+++ components/SubscripeCard.tsx
@@ -1,4 +1,31 @@
+"use client";
+
+import axios from "axios";
+import { useState } from "react";
+
 const SubscripeCard = () => {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/newsletter-signups`,
+        {
+          data: {
+            email,
+          },
+        }
+      );
+      // console.log(response.data.data.attributes);
+      alert("thank you for signing up for our news-letter");
+      setEmail("");
+    } catch (error: any) {
+      // console.log(error.response.data.error.message);
+      setEmail("");
+      alert(error.response.data.error.message);
+    }
+  };
+
   return (
     <div className=" bg-lightBrown rounded-lg flex gap-4 justify-center items-center mx-6 px-4 py-8 my-16">
       <div className="flex flex-col gap-6 w-[60%]">
@@ -10,11 +37,17 @@ const SubscripeCard = () => {
       </div>
       <div className="flex justify-center items-center w-[40%]">
         <input
-          type="text"
+          type="email"
+          required
           className="p-4 border"
           placeholder="Enter your E-mail address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
-        <button className="px-6 py-4 text-white font-semibold rounded-3xl bg-turquoise">
+        <button
+          onClick={handleSubmit}
+          className="px-6 py-4 text-white font-semibold rounded-3xl bg-turquoise"
+        >
           SUBSCRIBE
         </button>
       </div>
